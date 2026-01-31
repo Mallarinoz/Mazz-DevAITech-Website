ced1fff8eec6dfc8b7ebf82bc05bc116859b7871
Mallarinoz <mallarinoz@yahoo.com>
2026-01-31 01:10:32 -0500

    RESTABLECIMIENTO TOTAL: Sincronización exacta con repositorio privado

--- script.js
+++
@@ -1,5 +0,0 @@
-﻿function toggleTheme() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-}
-if (localStorage.getItem('theme') === 'dark') document.body.classList.add('dark-mode');
